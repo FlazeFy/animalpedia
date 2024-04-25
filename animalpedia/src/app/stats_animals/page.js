333492d3447bebce63348537818e5a2adc43dc23
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-04-25 22:48:40 +0700

    get total animal variety by country, get total animal pop by country

--- animalpedia/src/app/stats_animals/page.js
+++ animalpedia/src/app/stats_animals/page.js
@@ -9,6 +9,8 @@ import GetTotalAnimalCategory from "./usecases/get_total_animal_category"
 import GetTotalAnimalRegion from "./usecases/get_total_animal_region"
 import GetTotalAnimalStatus from "./usecases/get_total_animal_status"
 import GetTotalAnimalZone from "./usecases/get_total_animal_zone"
+import GetTotalAnimalVarietyByCountry from "./usecases/get_total_animal_variety_by_country"
+import GetTotalAnimalPopulationByCountry from "./usecases/get_total_animal_population_by_country"
 
 export default function StatsAnimals() {
     return <>
@@ -27,6 +29,12 @@ export default function StatsAnimals() {
                     </div>
                     <div className="col-lg-6 col-md-6 col-sm-12">
                         <GetTotalAnimalRegion ctx="get_total_animal_region"/>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <GetTotalAnimalVarietyByCountry ctx="get_total_animal_variety_by_country"/>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <GetTotalAnimalPopulationByCountry ctx="get_total_animal_population_by_country"/>
                     </div>
                 </div>
             </div>
